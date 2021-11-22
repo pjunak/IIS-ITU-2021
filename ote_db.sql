f938ea05a313dec840ff75bc5be893331325b8bd
Kristian Herman <kristianherman@seznam.cz>
2021-11-22 22:26:50 +0100

    nova databaze (napr NULL hodnoty)

--- ote_db.sql
+++ ote_db.sql
@@ -1,43 +1,24 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Počítač: localhost
--- Vytvořeno: Ned 21. lis 2021, 19:48
--- Verze serveru: 10.4.21-MariaDB
--- Verze PHP: 8.0.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Databáze: `ote_db`
---
 CREATE DATABASE IF NOT EXISTS `ote_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_czech_ci;
 USE `ote_db`;
-
--- --------------------------------------------------------
-
---
--- Struktura tabulky `firma`
---
 
 DROP TABLE IF EXISTS `firma`;
 CREATE TABLE `firma` (
   `rut_id` int(11) NOT NULL,
-  `ean` int(11) NOT NULL,
+  `ean` int(11) DEFAULT NULL,
   `nazev` varchar(128) COLLATE utf8_czech_ci NOT NULL,
-  `typ_firmy` enum('test') COLLATE utf8_czech_ci NOT NULL,
   `ic` int(11) NOT NULL,
-  `web` varchar(64) COLLATE utf8_czech_ci NOT NULL,
-  `email` varchar(64) COLLATE utf8_czech_ci NOT NULL,
+  `dic` int(11) DEFAULT NULL,
+  `web` varchar(64) COLLATE utf8_czech_ci DEFAULT NULL,
+  `email` varchar(64) COLLATE utf8_czech_ci DEFAULT NULL,
   `datum_vytvoreni` date NOT NULL,
   `ulice` varchar(32) COLLATE utf8_czech_ci NOT NULL,
   `cislo_p` varchar(8) COLLATE utf8_czech_ci DEFAULT NULL,
@@ -46,21 +27,10 @@ CREATE TABLE `firma` (
   `psc` int(11) NOT NULL,
   `predcisli` int(11) DEFAULT NULL,
   `cislo_uctu` int(32) NOT NULL,
-  `kod_banky` enum('0100') COLLATE utf8_czech_ci NOT NULL
+  `kod_banky` enum('0100','0300','0600','0710','0800','2010','2070','2100','2250','2260','2600','2700','3030','3040','3050','3500','4000','4300','5500','5800','6000','6100','6210','6300','6700','6800','7950','7960','7970','7980','7990','8060','8090','8211') COLLATE utf8_czech_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
---
--- Vypisuji data pro tabulku `firma`
---
-
-INSERT INTO `firma` (`rut_id`, `ean`, `nazev`, `typ_firmy`, `ic`, `web`, `email`, `datum_vytvoreni`, `ulice`, `cislo_p`, `cislo_o`, `obec`, `psc`, `predcisli`, `cislo_uctu`, `kod_banky`) VALUES(123456789, 123456789, 'Firma TEST s.r.o.', 'test', 123456, 'http://test.com', 'test@domena.cz', '2021-11-21', 'Testová', '1024', '8', 'Testov', 12345, NULL, 123456789, '0100');
-INSERT INTO `firma` (`rut_id`, `ean`, `nazev`, `typ_firmy`, `ic`, `web`, `email`, `datum_vytvoreni`, `ulice`, `cislo_p`, `cislo_o`, `obec`, `psc`, `predcisli`, `cislo_uctu`, `kod_banky`) VALUES(234567891, 234567891, 'Ravoz spol. s r.o.', 'test', 11111122, 'http://ravoz.cz', 'ravoz@ravoz.cz', '2021-11-21', 'Tovární', NULL, '40', 'Olomouc', 77900, NULL, 1111111111, '0100');
-
--- --------------------------------------------------------
-
---
--- Struktura tabulky `osoba`
---
+INSERT INTO `firma` (`rut_id`, `ean`, `nazev`, `ic`, `dic`, `web`, `email`, `datum_vytvoreni`, `ulice`, `cislo_p`, `cislo_o`, `obec`, `psc`, `predcisli`, `cislo_uctu`, `kod_banky`) VALUES(123456789, 123456789, 'Firma TEST s.r.o.', 123456, NULL, 'http://test.com', 'test@domena.cz', '2021-11-21', 'Testová', '1024', '8', 'Testov', 12345, NULL, 123456789, '0100');
 
 DROP TABLE IF EXISTS `osoba`;
 CREATE TABLE `osoba` (
@@ -72,27 +42,12 @@ CREATE TABLE `osoba` (
   `telefon` int(11) NOT NULL,
   `email` varchar(32) COLLATE utf8_czech_ci NOT NULL,
   `heslo` bigint(20) NOT NULL,
-  `ulice` varchar(32) COLLATE utf8_czech_ci NOT NULL,
-  `cislo_p` varchar(8) COLLATE utf8_czech_ci NOT NULL,
-  `cislo_o` varchar(8) COLLATE utf8_czech_ci NOT NULL,
-  `obec` varchar(32) COLLATE utf8_czech_ci NOT NULL,
-  `psc` int(11) NOT NULL,
-  `kancelar` varchar(32) COLLATE utf8_czech_ci NOT NULL,
+  `kancelar` varchar(32) COLLATE utf8_czech_ci DEFAULT NULL,
   `pozice` varchar(32) COLLATE utf8_czech_ci NOT NULL,
   `plat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
---
--- Vypisuji data pro tabulku `osoba`
---
-
-INSERT INTO `osoba` (`id`, `id_ucastnika`, `typ_osoby`, `jmeno`, `prijmeni`, `telefon`, `email`, `heslo`, `ulice`, `cislo_p`, `cislo_o`, `obec`, `psc`, `kancelar`, `pozice`, `plat`) VALUES(1, 123, 'disponent', 'Testovič', 'Test', 987654321, 'urednik@domena.cz', 123, 'Rúžová', '', '4', 'Rúženín', 23456, '1.20A', 'vedoucí sekce HR', 37000);
-
--- --------------------------------------------------------
-
---
--- Struktura tabulky `pozadavek`
---
+INSERT INTO `osoba` (`id`, `id_ucastnika`, `typ_osoby`, `jmeno`, `prijmeni`, `telefon`, `email`, `heslo`, `kancelar`, `pozice`, `plat`) VALUES(1, 123, 'disponent', 'Testovič', 'Test', 987654321, 'urednik@domena.cz', 123, '1.20A', 'vedoucí sekce HR', 37000);
 
 DROP TABLE IF EXISTS `pozadavek`;
 CREATE TABLE `pozadavek` (
@@ -102,14 +57,8 @@ CREATE TABLE `pozadavek` (
   `predmet` varchar(128) COLLATE utf8_czech_ci NOT NULL,
   `status` enum('test') COLLATE utf8_czech_ci NOT NULL,
   `obsah_pozadavku` text COLLATE utf8_czech_ci NOT NULL,
-  `odpoved` text COLLATE utf8_czech_ci NOT NULL
+  `odpoved` text COLLATE utf8_czech_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabulky `vykaz`
---
 
 DROP TABLE IF EXISTS `vykaz`;
 CREATE TABLE `vykaz` (
@@ -123,12 +72,6 @@ CREATE TABLE `vykaz` (
   `spotreba_z_toho_lokalni` int(11) NOT NULL,
   `spotreba_z_toho_odber` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabulky `vyrobna`
---
 
 DROP TABLE IF EXISTS `vyrobna`;
 CREATE TABLE `vyrobna` (
@@ -158,59 +101,29 @@ CREATE TABLE `vyrobna` (
   `datum_uvedeni_do_provozu` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
---
--- Indexy pro exportované tabulky
---
 
---
--- Indexy pro tabulku `firma`
---
 ALTER TABLE `firma`
   ADD PRIMARY KEY (`rut_id`);
 
---
--- Indexy pro tabulku `osoba`
---
 ALTER TABLE `osoba`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexy pro tabulku `pozadavek`
---
 ALTER TABLE `pozadavek`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexy pro tabulku `vyrobna`
---
 ALTER TABLE `vyrobna`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT pro tabulky
---
 
---
--- AUTO_INCREMENT pro tabulku `firma`
---
 ALTER TABLE `firma`
   MODIFY `rut_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=234567892;
 
---
--- AUTO_INCREMENT pro tabulku `osoba`
---
 ALTER TABLE `osoba`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT pro tabulku `pozadavek`
---
 ALTER TABLE `pozadavek`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT pro tabulku `vyrobna`
---
 ALTER TABLE `vyrobna`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
